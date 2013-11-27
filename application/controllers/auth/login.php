1acdf266571e9abe1cb378cc33286807391ed0c3
Sheldon Rong <sheldon@debian>
2013-11-28 08:01:33 +1100

    more build for login box fixes permissions for all bootstrap less assets

--- application/controllers/auth/login.php
+++ application/controllers/auth/login.php
@@ -1,10 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-
     public function index() {
-        
         $this->load->view('login');
+    }
+
+    public function submit(){
+
+        $name = $this->input->post('name');
+        $is_ajax=$this->input->post('ajax');
+        $data['main_content']='contact_form_thanks';
+
+        if($is_ajax){
+            $this->load->view($data['main_content']);
+        }else{
+            $this->load->view('includes/template', $data);
+        }
+
     }
 }
 
